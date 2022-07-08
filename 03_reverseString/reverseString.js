7d9f44aabdb2020608fe6b1704929823d823776a
HannahCunliffe <hannahcunliffe01@googlemail.com>
2022-07-08 17:30:46 +0100

    completed reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    //split string into individual characters in an array
+    let reversedString = '';
+    const chars = string.split('');
+    //loop through backwards from the last character, find that in the array, join to output string
+    for (i = string.length; i > 0; i--) {
+        let letter = chars[i - 1];
+        reversedString += letter;
+    };
+
+    //checked the provided solution, they came up with a far more succint version:
+    //other person's solution code below for future reference:
+    // return string.split('').reverse().join('');
+    return reversedString;
 
 };
 
