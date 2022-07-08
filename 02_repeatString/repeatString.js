1b2e0eb903c4426453d096fd42595031d049f18e
HannahCunliffe <hannahcunliffe01@googlemail.com>
2022-07-08 16:45:54 +0100

    completed repeat string exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    // returns error if negative number entered
+    if (number < 0) {
+        return("ERROR");
+    };
+    // initialises the final output variable, loops and adds a repeat of the string to it
+    // for the number of times entered into the function
+    let outputString = '';
+    for (let i = 0; i < number; i++) {
+        outputString += string;
+    };
+    return outputString;
 };
 
 // Do not edit below this line
