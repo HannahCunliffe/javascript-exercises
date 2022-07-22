271226fca322ce3f24358203c5a08d9981434d2b
HannahCunliffe <hannahcunliffe01@googlemail.com>
2022-07-22 14:55:59 +0100

    condensed code into a more readable format

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,33 @@
 const leapYears = function(inputYear) {
-    if (inputYear % 4 == 0) {
-        if (inputYear % 100 == 0) {
-            if (inputYear % 400 == 0) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
+
+// old version:
+ //   if (inputYear % 4 == 0) {
+  //      if (inputYear % 100 == 0) {
+  //          if (inputYear % 400 == 0) {
+  //              return true;
+ //           }
+ //           else {
+ //               return false;
+  //          }
+  //      }
+ //       return true;
+ //   }
+ //   else {
+//        return false;
+//    };
+
+
+//condensed version:
+// checks if input year divides by 4, then also checks if it is not divisible by 100 with 
+// it being divisble by 400 the exception which is a leap year
+    if (inputYear % 4 == 0 && (inputYear % 100 !== 0 || inputYear % 400 == 0)) {
         return true;
     }
     else {
         return false;
     };
+
+
 };
 
 // Do not edit below this line
