b56963b3a2b34e41c02c527c6d832db987e03f01
HannahCunliffe <hannahcunliffe01@googlemail.com>
2022-10-22 15:06:09 +0100

    completed calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,56 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	const total = num1 + num2;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	const total = num1 - num2;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  let i = 0;
+
+  if (array.length == 0) {
+    return 0;
+  }
+
+  do {
+    total = total + array[i];
+    i++;
+  } while (i < array.length);
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 0;
+  let firstNum = array[0];
+  total = firstNum;
 
+  for (let i = 1; i < array.length; i++) {
+    total = total * array[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let i = num;
+  let total = 1;
+
+  if (num == 0 || num == 1) {
+    return 1
+  } else {
+    for (let i = num; i >= 1; i--) {
+      total = total * i;
+    }
+    return total;
+  }
+  
 };
 
 // Do not edit below this line
