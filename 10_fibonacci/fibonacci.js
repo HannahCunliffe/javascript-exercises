30e61a009a896071ce9ddc746b4736a280373593
HannahCunliffe <hannahcunliffe01@googlemail.com>
2022-10-22 17:40:28 +0100

    completed all functions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(numToReturn) {
+    let nextNum = 0;
+    let num1 = 0;
+    let num2 = 1;
 
+    if (numToReturn < 0 ) {
+        return "OOPS";
+    }
+
+    numToReturn = parseInt(numToReturn);
+
+    for (let i = 1; i <= numToReturn; i++) {
+        nextNum = num1 + num2;
+        num1 = num2;
+        num2 = nextNum;
+    }
+
+    return num1;
+    
 };
 
 // Do not edit below this line
