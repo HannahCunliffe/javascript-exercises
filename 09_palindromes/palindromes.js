99692c42fac5d46e36a436230db1e10ca0c5ba3e
HannahCunliffe <hannahcunliffe01@googlemail.com>
2022-10-22 17:24:26 +0100

    completed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+   
+    //need to convert original string and reversed output to lower case
+    //remove all punctuation and spaces from both strings
+
+    const stringLowerCase = string.toLowerCase();
+
+    let processedString = stringLowerCase.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+
+    /* removes spaces */
+                          .replace(/\s+/g,'');
+
+    const stringArray = processedString.split('');
+
+    //uses the spread operator to make a shallow copy of the array for reversal
+    const reversedStringArray = [...stringArray]
+                                .reverse();
+
+    const originalString = stringArray.toString();
+
+    const reversedString = reversedStringArray.toString();
+
+  console.log(stringArray, + " " + "reversed: " + reversedStringArray);
+
+  /* console.log(originalString + " " + "reversed: " + reversedString) */
+
+    if (originalString == reversedString) {
+        return true;
+    } else {
+        return false;
+    };
 
 };
 
