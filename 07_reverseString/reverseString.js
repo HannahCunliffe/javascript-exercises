15a8b681c31e858c304010fb00d6e74f3e283e4e
Hannah Cunliffe <58406367+HannahCunliffe@users.noreply.github.com>
2025-07-11 16:08:47 +0100

    completed exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = Array.from(string);
+    stringArray = stringArray.reverse();
+    return stringArray.join('');
 };
 
 // Do not edit below this line
