250aaa31664ef721241f11b78c7c9b2e46623284
Hannah Cunliffe <58406367+HannahCunliffe@users.noreply.github.com>
2025-07-11 16:48:16 +0100

    completed exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+//returns an error if the parameters are negative numbers or non-integers
+if (num1 < 0 || num2 < 0) {
+    return 'ERROR'
+} else if (Number.isInteger(num1) === false || Number.isInteger(num2) === false) {
+    return 'ERROR'
+}
+
+let total = 0;
+
+let highestNumber = 0;
+let lowestNumber = 0;
+
+//checks which number is higher and assigns values based on those results
+// this ensures that the integers between the two numbers are properly summed by the loop
+if (num1 > num2) {
+    highestNumber = num1;
+    lowestNumber = num2;
+} else {
+    highestNumber = num2;
+    lowestNumber = num1;
+}
+
+for (let index = lowestNumber; index <= highestNumber; index++) {
+    total+= index;
+}
+
+
+return total;
+
 
 };
 
