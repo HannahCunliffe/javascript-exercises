0c38b662cf080c680b75490153d659620114c759
Hannah Cunliffe <58406367+HannahCunliffe@users.noreply.github.com>
2025-07-17 19:27:40 +0100

    completed exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let userInput = Number(input);
 
+    if (userInput < 0) {
+        return "OOPS"
+    }
+
+    if (userInput === 0) {
+        return 0;
+    } else if (userInput === 1) {
+        return 1;
+    }
+
+    let result;
+    let num1 = 0;
+    let num2 = 1;
+
+    for (let i = 2; i <= userInput; i++) {
+        result = num1 + num2;
+        num1 = num2;
+        num2 = result;
+    }
+
+    return num2
 };
 
 // Do not edit below this line
