4dde3886cf34284fd3ecfcf4cdf96ab2a53d6981
Hannah Cunliffe <58406367+HannahCunliffe@users.noreply.github.com>
2025-07-17 18:27:54 +0100

    completed exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    
+    //changes string to lowercase, removes spaces and non-alphanumerical
+    //characters so that the sentence can be properly compared to a reversed
+    //version of itself
+
+    let originalString = string
+    .toLowerCase()
+    //regex to  remove alphanumeric characters and replace with empty spaces
+    .replace(/[^a-zA-Z0-9]/g, '')
+    .split(" ")
+    .join(" ")
+    ;
+
+    let reversedString = originalString
+    .split('')
+    .reverse()
+    .join('');
+    
+
+    if (reversedString === originalString) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
