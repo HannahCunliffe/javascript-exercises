4b5f206ee3e9a544c0e19fc379641fa021db59b2
Hannah Cunliffe <58406367+HannahCunliffe@users.noreply.github.com>
2025-07-17 17:46:06 +0100

    completed exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((currentTotal, item) => {
+    //returns the result of the calculation to be used as the total value for the next 
+    //item that is being iterated through
+    return currentTotal += item;
+  }, 0)
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  
+  let total = 1;
+  
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+
+  return total;
+
 };
 
 // Do not edit below this line
