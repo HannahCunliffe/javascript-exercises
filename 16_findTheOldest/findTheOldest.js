3659d1aaae6e3587267d1906820ff1f493b8f273
Hannah Cunliffe <58406367+HannahCunliffe@users.noreply.github.com>
2025-07-18 15:01:36 +0100

    completed exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+
+const findTheOldest = function (people) {
+    filteredArray = people
+        //checks if year of death exists, if not calculates age based on current date
+        // and birth date, then adds a new key value pair to the object with the person's age
+        .map(person => {
+            if (!(person.yearOfDeath)) {
+                const date = new Date();
+                let currentYear = date.getFullYear();
+                person.age = currentYear - person.yearOfBirth
+            } else {
+                person.age = person.yearOfDeath - person.yearOfBirth;
+            }
+
+            return person;
+        })
+        //then sorts the list based on people's age descending from oldest to youngest
+        .sort((personA, personB) => {
+            return personB.age - personA.age
+        })
+
+    //returns the first element in the array since that will be the oldest person
+    //after it has been sorted
+    return filteredArray[0]
 
 };
 
