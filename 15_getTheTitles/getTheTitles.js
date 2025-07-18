6c9d552831baed0d4deaf5d289f58f7dd2da60b5
Hannah Cunliffe <58406367+HannahCunliffe@users.noreply.github.com>
2025-07-18 12:46:47 +0100

    completed exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    let titleArray = bookArray.map((item) => {
+        return item.title;
+    });
 
-};
+    return titleArray;
+
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
